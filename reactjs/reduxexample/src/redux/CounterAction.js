febd84dd5ea09e7aadfbdfe246a42b064b209600
Rinkal <rinkaladesara.tops@gmail.com>
2025-04-14 11:42:34 +0530

    redux

---
+++ reactjs/reduxexample/src/redux/CounterAction.js
@@ -0,0 +1,13 @@
+export const inc = 'INCREMENT'
+export const dec = 'DECREMENT'
+export const add = ()=>{
+    return {
+        type:inc
+    }
+}
+export const minus = ()=>{
+    return {
+        type:dec
+    }
+}
+// export default {add,minus}
